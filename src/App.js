b3ef2f0581172efbef8c1a1932024eb82070dbb9
Carmelo Rubiolo <melo_rubiolo@hotmail.com>
2023-02-05 12:32:54 -0300

    agregado de componente ConfAsist

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Portada from "./components/Portada/Portada";
 import CuentaRegresiva from "./components/CuentaRegresiva/CuentaRegresiva.jsx";
 import Ubicacion from "./components/Ubicacion/Ubicacion.jsx"
 import Fotos from "./components/Fotos/Fotos.jsx"
+import ConfirmarAsistencia from "./components/ConfirmarAsistencia/ConfirmarAsistencia"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <CuentaRegresiva />
       <Ubicacion />
       <Fotos />
+      <ConfirmarAsistencia />
     </>
   );
 }
---
+++ src/components/ConfirmarAsistencia/ConfirmarAsistencia.jsx
@@ -0,0 +1,16 @@
+import "./ConfirmarAsistencia.css"
+
+const ConfirmarAsistencia = () => {
+    return (
+        <>
+            <h1 className="text-center mt-5">Confirmar Asistencia</h1>
+            <div className="btn-container">
+                <button>
+                    
+                    <a href="https://docs.google.com/forms/d/e/1FAIpQLSdfhsYeQw4LNG3CChPkeYF0bY924tDgCSCCGK4DlSWjpKyAiQ/viewform?usp=sf_link"><span class="TxtEffect">Si Voy</span></a>
+                </button>
+            </div>
+        </>
+    )
+}
+export default ConfirmarAsistencia
